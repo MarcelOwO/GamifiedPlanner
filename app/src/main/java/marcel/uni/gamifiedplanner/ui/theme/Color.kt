a211b1d2085e991b39a4c294e1fa2f41782eb3bc
MarcelOwO <Marcel-sigl@gmx.de>
2025-11-13 19:43:22 +0100

    update color scheme

--- app/src/main/java/marcel/uni/gamifiedplanner/ui/theme/Color.kt
+++ app/src/main/java/marcel/uni/gamifiedplanner/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package marcel.uni.gamifiedplanner.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryDark = Color(0xF1C388FF)
+val DarkDark = Color(0x13171FFF)
+val DarkGrayDark =Color(0x3A455CFF)
+val MediumGrayDark =Color(0x8D97ACFF)
+val LightGrayDark =Color(0xE3ECFEFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryLight = Color(0x44CDFAFF)
+val LightLight = Color(0x44CDFAFF)
+val DarkGrayLight = Color(0x818181FF)
+val MediumGrayLight = Color(0xB4B4B4FF)
+val LightGrayLight = Color(0xD8D8D8FF)
--- app/src/main/java/marcel/uni/gamifiedplanner/ui/theme/Theme.kt
+++ app/src/main/java/marcel/uni/gamifiedplanner/ui/theme/Theme.kt
@@ -11,15 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryDark,
+    secondary = LightGrayDark,
+    tertiary = MediumGrayDark,
+    background = DarkDark,
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryLight,
+    secondary = LightGrayLight,
+    tertiary = MediumGrayLight,
+    background = LightLight,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -35,7 +38,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun GamifiedPlannerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
